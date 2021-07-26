cd55a63033e122d8f7b4d907d65a2e2e22d49d30
Your Name <you@example.com>
2021-07-26 19:50:43 +1000

    Crud localy created

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -18,7 +18,8 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id){
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
             
             return await Mediator.Send(new Details.Query{Id = id});
 
